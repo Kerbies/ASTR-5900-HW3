5565dd81cb2171ed5cf858171104bbef28f9e08f
Lennon Kirby <lennon.rivera0@gmail.com>
2026-02-24 10:39:56 -0600

    Import RK4 and Define Max_Boltz Dist.

--- Boltzmann_Distribution.py
+++ Boltzmann_Distribution.py
@@ -1,5 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 def main():
     pass
+
+# T --- Tempurature in Kelvin
+# m --- Mass of the particle in kg
+# v --- Speed of the particle in m/s
+def Maxwell_Boltzmann_Distribution(v, T, m):
+
+    # Boltzmann constant in J/K
+    k_B = 1.380649e-23 
+    
+    # Maxwell-Boltzmann distribution formula 
+    f = (m / (2 * np.pi * k_B * T))**(3/2) * 4 * np.pi * v**2 * np.exp(-m * v**2 / (2 * k_B * T))
+    
+    return f
+
+def RK4(f, y0, t0, tf, h):
+
+    # Defines the time array
+    t = np.arange(t0, tf+h, h)
+    
+    # Initialize the solution array
+    y = np.zeros_like(t)
+
+    # Set initial condition
+    y[0] = y0
+
+    # RK4 Algorithm
+    for i in range(1, len(t)):
+        k1 = f(t[i-1], y[i-1])
+        k2 = f(t[i-1] + h/2, y[i-1] + k1*h/2)
+        k3 = f(t[i-1] + h/2, y[i-1] + k2*h/2)
+        k4 = f(t[i-1] + h  , y[i-1] + k3*h  )
+
+        y[i] = y[i-1] + h*(k1 + 2*k2 + 2*k3 + k4)/6
+
+    return t, y
 
 if __name__ == "__main__":
     main()
